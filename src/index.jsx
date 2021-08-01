2090b05bf7b94a26a41e318e219c8fa6cde7a258
Akornefa Akyea <64564297+akornefa@users.noreply.github.com>
2021-07-31 22:47:47 -0500

    Import createStore to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import { MainView } from './components/main-view/main-view';
 import Container from 'react-bootstrap/Container';
 
